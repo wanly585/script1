eea579ee75e80b0b14b96f5c99428e035b56aa53
wanly585 <wamllypareggy@gmail.com>
2025-08-31 18:03:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function cambiarTitulo1() {
+      document.getElementById("titulo").textContent = "soleado";
+      document.body.style.backgroundColor = "yellow";
+    }
+
+function cambiarTitulo2() {
+      document.getElementById("titulo").textContent = "lluvioso";
+      document.body.style.backgroundColor = "cadetblue";
+      
+    }
+
+function cambiarTitulo3() {
+      document.getElementById("titulo").textContent = "nublado";
+      document.body.style.backgroundColor = "gray";
+
+    }   
+    
